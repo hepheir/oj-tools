6b038a859b09b416d785f04a6d8b22937e97684e
Hepheir <hepheir@gmail.com>
2024-05-16 03:42:11 +0900

    feat!: add id auto increment for TestCase

--- src/oj.py
+++ src/oj.py
@@ -20,30 +20,61 @@ INTEGER_SIZE = 2147483648
 
 
 class TestCase:
-    def __init__(self, id: str, input: str, output: str, strip=True) -> None:
-        self.id = id
-        self.input = input.strip() if strip else input
-        self.output = output.strip() if strip else output
+    __auto_increment__ = 0
+
+    @classmethod
+    def _auto_id(cls) -> int:
+        cls.__auto_increment__ += 1
+        return cls.__auto_increment__
+
+    def __init__(self, id: str = None, auto_increment: bool = True) -> None:
+        if id is None:
+            assert auto_increment, "provide id, or enable auto_increment"
+            id = str(self._auto_id())
+        self._id = id
+        self._input = ''
+        self._output = ''
+
+    def __hash__(self) -> int:
+        return hash(self._id)
+
+    @property
+    def input(self) -> str:
+        """content of input data."""
+        return self._input
+
+    @input.setter
+    def input(self, s: str) -> None:
+        self._input = s
+
+    @property
+    def output(self) -> str:
+        """content of output data."""
+        return self._output
+
+    @output.setter
+    def output(self, s: str) -> None:
+        self._output = s
 
     @property
     def input_size(self) -> int:
         """Length of input data."""
-        return len(self.input)
+        return len(self._input)
 
     @property
     def output_size(self) -> int:
         """Length of output data."""
-        return len(self.output)
+        return len(self._output)
 
     @property
     def input_name(self) -> str:
         """A filename for the input."""
-        return f'{self.id}.in'
+        return f'{self._id}.in'
 
     @property
     def output_name(self) -> str:
         """A filename for the output."""
-        return f'{self.id}.out'
+        return f'{self._id}.out'
 
     @property
     def stripped_output_md5(self) -> str:
