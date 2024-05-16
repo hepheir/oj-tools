64ef39f48411cdb71e8a5aa843359297c9502f4b
Hepheir <hepheir@gmail.com>
2024-05-16 18:34:06 +0900

    feat: TestCase에 TextIOWrapper 사용

--- src/oj/problem.py
+++ src/oj/problem.py
@@ -1,48 +1,48 @@
 from pathlib import Path
+import collections
 import json
 import os
 import tempfile
 import typing
 import zipfile
 
-from testcase import TestCase
+from oj.testcase import TestCase
 
 
 class Problem:
     def __init__(self, title: str) -> None:
         self.title = title
         self.spj = False
-        self.testcases: typing.Dict[str, TestCase] = {}
+        self.testcases: typing.Dict[str, TestCase] = collections.defaultdict(TestCase)
 
-    def add_testcase(self, testcase: TestCase):
-        """Add a testcase for this problem."""
-        self.testcases[testcase] = testcase
-
-    def extract_as_dir(self, dirname: typing.Optional[str]=None, in_ext='.in', out_ext='.out') -> None:
+    def extract_as_dir(self, dirname: typing.Optional[str]=None, in_ext='.in', out_ext='.out', info='info') -> None:
         """Save problem in a directory.
 
         dirname: default is `./PROBLEM_TITLE`
         """
         dir_path = Path(dirname) if dirname is not None else Path(f'./{self.title}')
-        info_filename = 'info'
+        data = {
+            "spj": self.spj,
+            "testcases": {},
+        }
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        for testcase in self.testcases.values():
-            testcase.input.extract_as_file(dir_path / (testcase.id+in_ext))
-            testcase.output.extract_as_file(dir_path / (testcase.id+out_ext))
-        with open(dir_path / info_filename, 'w') as f:
-            json.dump({
-                "spj": self.spj,
-                "testcases": {
-                    testcase.id: {
-                        "stripped_output_md5": testcase.stripped_output_md5,
-                        "input_size": testcase.input_size,
-                        "output_size": testcase.output_size,
-                        "input_name": (testcase.id+in_ext),
-                        "output_name": (testcase.id+out_ext),
-                    } for testcase in self.testcases.values()
-                },
-            }, f, ensure_ascii=True)
+        for id, tc in self.testcases.items():
+            input_name = id+in_ext
+            output_name = id+out_ext
+            with open(dir_path / input_name, 'w') as f:
+                f.write(tc.input_text)
+            with open(dir_path / output_name, 'w') as f:
+                f.write(tc.output_text)
+            data['testcases'][id] = {
+                "stripped_output_md5": tc.stripped_output_md5,
+                "input_size": tc.input_size,
+                "output_size": tc.output_size,
+                "input_name": input_name,
+                "output_name": output_name,
+            }
+        with open(dir_path / info, 'w') as f:
+            json.dump(data, f, ensure_ascii=True)
 
     def extract_as_zip(self, filename: typing.Optional[str]=None) -> None:
         """Save problem as .zip file
--- src/oj/testcase.py
+++ src/oj/testcase.py
@@ -1,86 +1,54 @@
-from pathlib import Path
 import hashlib
-import textwrap
-import typing
-
-class TestCaseIO:
-    def __init__(self) -> None:
-        self._content = ''
-
-    def __str__(self) -> str:
-        return self._content
-
-    def __len__(self) -> int:
-        return len(self._content)
-
-    def from_args(self, *args: typing.Tuple[typing.Any], sep:str=' '):
-        self._content = sep.join(map(str, args))
-
-    def from_text(self, text: str, dedent=False, strip=True):
-        self._content = text
-        self._post_process(dedent=dedent, strip=strip)
-
-    def from_file(self, file_path: str, dedent=False, strip=True):
-        with open(file_path, 'r') as f:
-            self._content = f.read()
-        self._post_process(dedent=dedent, strip=strip)
-
-    def extract_as_file(self, file_path: str) -> None:
-        with open(file_path, 'w') as f:
-            f.write(self._content)
-
-    def _post_process(self, dedent: bool, strip: bool) -> None:
-        if dedent:
-            self._content = textwrap.dedent(self._content)
-        if strip:
-            self._content = self._content.strip()
+import io
 
 
 class TestCase:
-    __auto_increment__ = 0
-
-    @classmethod
-    def _auto_id(cls) -> int:
-        cls.__auto_increment__ += 1
-        return cls.__auto_increment__
-
-    def __init__(self, id: str = None, auto_increment: bool = True) -> None:
-        if id is None:
-            assert auto_increment, "provide id, or enable auto_increment"
-            id = str(self._auto_id())
-        self._id = id
-        self._input = TestCaseIO()
-        self._output = TestCaseIO()
-
-    def __hash__(self) -> int:
-        return hash(self._id)
+    def __init__(self) -> None:
+        self._input = io.TextIOWrapper(io.BytesIO())
+        self._output = io.TextIOWrapper(io.BytesIO())
 
     @property
-    def id(self) -> str:
-        return self._id
-
-    @property
-    def input(self) -> TestCaseIO:
-        """input data."""
+    def input(self) -> io.TextIOWrapper:
+        """input stream."""
         return self._input
 
     @property
-    def output(self) -> TestCaseIO:
-        """output data."""
+    def output(self) -> io.TextIOWrapper:
+        """output stream."""
         return self._output
+
+    @property
+    def input_text(self) -> str:
+        """input data."""
+        return self._read_stream(self._input)
+
+    @property
+    def output_text(self) -> str:
+        """output data."""
+        return self._read_stream(self._output)
 
     @property
     def input_size(self) -> int:
         """Length of input data."""
-        return len(self._input)
+        return self._size_of_stream(self._input)
 
     @property
     def output_size(self) -> int:
         """Length of output data."""
-        return len(self._output)
+        return self._size_of_stream(self._output)
 
     @property
     def stripped_output_md5(self) -> str:
         md5 = hashlib.md5()
-        md5.update(str(self.output).strip().encode())
+        md5.update(str(self.output_text).strip().encode())
         return md5.hexdigest()
+
+    def _read_stream(self, stream: io.TextIOBase) -> str:
+        cursor = stream.tell()
+        stream.seek(0, io.SEEK_SET)
+        text = stream.read()
+        stream.seek(cursor, io.SEEK_SET)
+        return text
+
+    def _size_of_stream(self, stream: io.TextIOBase) -> str:
+        return len(self._read_stream(stream))
